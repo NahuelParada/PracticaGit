588fcf1853d014c5517e8429885bc1fb689c64c0
NahuelParada <131213462+NahuelParada@users.noreply.github.com>
2026-09-08 21:12:22 -0300

    Creacion de carpetas para mayor orden y creacion de enums Hambre,Higene y Salud y creacion de Clase Animal

---
+++ src/Clases/Animal.java
@@ -0,0 +1,13 @@
+package Clases;
+
+import Enums.Hambre;
+
+public abstract class Animal {
+    private String nombre;
+    private String especie;
+    private Hambre hambre;
+    private int salud;
+    private  higene;
+
+
+}
